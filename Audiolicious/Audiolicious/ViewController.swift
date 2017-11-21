182b67017d5a0228f9037878a96f0bde877712a3
callumdmay <callumdmay@gmail.com>
2017-11-21 00:28:49 -0500

    Add basic BLE scanning

--- Audiolicious/Audiolicious/ViewController.swift
+++ Audiolicious/Audiolicious/ViewController.swift
@@ -7,19 +7,48 @@
 //
 
 import UIKit
+import CoreBluetooth
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, CBCentralManagerDelegate, CBPeripheralDelegate {
+    @IBOutlet weak var connectionStatusLabel: UILabel!
+    
+    var centralManager: CBCentralManager!
+    var peripheral:CBPeripheral!
+    
+    let BEAN_NAME = "Robu"
+    let BEAN_SCRATCH_UUID =
+        CBUUID(string: "a495ff21-c5b1-4b44-b512-1370f02d74de")
+    let BEAN_SERVICE_UUID =
+        CBUUID(string: "a495ff20-c5b1-4b44-b512-1370f02d74de")
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        centralManager = CBCentralManager(delegate: self, queue: nil)
+    }
+    
+    func centralManagerDidUpdateState(_ central: CBCentralManager) {
+        if (central.state == .poweredOn) {
+            self.centralManager?.scanForPeripherals(withServices: nil, options: nil)
+            self.connectionStatusLabel.text = "Scanning..."
+        } else {
+            print("BLE not on")
+        }
+    }
+    
+    func centralManager(_ central: CBCentralManager,
+                                 didDiscover peripheral: CBPeripheral,
+                                 advertisementData: [String : Any],
+                                 rssi RSSI: NSNumber) {
+        self.connectionStatusLabel.text = "Found peripheral!"
     }
 
+    @IBAction func onRefresh(_ sender: UIButton) {
+        
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
 }
 
